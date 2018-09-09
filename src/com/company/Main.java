69449cd0ac5552604149e2c5fd700fcc6f019456
Valentina Kononenko <valentina@kononenko.net>
2018-09-09 14:46:59 -0400

    This was a triumph

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner Reader = new Scanner(System.in);
+	System.out.print("What is your first name?");
+	String fName = Reader.next();
+	System.out.print("What is your last name?");
+	String lName = Reader.next();
+
+	System.out.print("Your full name is " + fName + " " + lName + ".");
     }
 }
